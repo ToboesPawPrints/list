b4004caf00ca9661702e752fa182db9dc35e7215
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-07-31 15:16:58 -0700

    Added FilterButton component in place of the JSX for FilterButton.

--- src/App.js
+++ src/App.js
@@ -19,16 +19,9 @@ function App(props) {
      
       <div className="filters btn-group stack-exception">
 
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+<FilterButton />
+<FilterButton />
+<FilterButton />
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
