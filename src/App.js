b48845332c7d4364615b4ab48ecb55d3c0dc7f97
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-07-31 15:11:27 -0700

    Added component imports for Form and FilterButton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Todo from "./components/Todo.js";
+import Form from "/.components/Form";
+import FilterButton from "/.components/FilterButton";
 
 function App(props) {
   const taskList = props.tasks.map((task) => (
@@ -14,29 +16,9 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+     
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+
         <button type="button" className="btn toggle-btn" aria-pressed="false">
           <span className="visually-hidden">Show </span>
           <span>Active</span>
