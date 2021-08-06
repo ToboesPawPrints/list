fe940e3128b4f82371338da109e7025d2d3345f2
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-08-05 19:29:31 -0700

    added props to delete button

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
+  function deleteTask(id) {
+    console.log(id)
+  }
+
   const taskList = tasks.map(task => (
     <Todo
         id={task.id}
@@ -25,6 +29,7 @@ function App(props) {
         completed={task.completed}
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
       />
     )
   );
