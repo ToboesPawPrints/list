2e3aeabda2491a521b3508357bcf0fe5c3796046
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-08-09 19:26:20 -0700

    Served up public folder and all contents.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
+ const path = require('path');
  const express = require('express');
  const app = express();
 
- 
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
