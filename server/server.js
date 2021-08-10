69b18c96703b16c66ba9cfc8a0fe158613a78e2f
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-08-09 19:28:24 -0700

    Added a console log to test.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
  const path = require('path');
  const express = require('express');
  const app = express();
-
-const publicPath = path.join(__dirname, '..', 'public');
+ const port = process.env.PORT || 3000;
+ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`The server is up on port $port!`);
+});
