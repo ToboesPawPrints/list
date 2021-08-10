ca4ade94abd3de2a064c4ca55c57d685999bdd43
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-08-09 19:32:43 -0700

    Added a get to ensure index.html is being served.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@
 
 app.use(express.static(publicPath));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`The server is up on port ${port}!`);
 });
